13cc22d464bf760e71dc156cdbf2503cbcca4332
cryxservices-glitch <cryxservices@gmail.com>
2026-05-15 01:51:46 -0400

    simplified

--- api/track.js
+++ api/track.js
@@ -4,9 +4,9 @@ module.exports = function handler(req, res) {
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || 'unknown';
   const ua = req.headers['user-agent'] || 'unknown';
   
-  const webhook = 'https://discord.com/api/webhooks/1464006669796118610/NtLug816A6N5psWgUjMrUBFe-yRtvmPvmIC8hMrbORLuDrxP4QnErus1K5XH2pKlqXi9';
+  const data = JSON.stringify({content: `View ${ip}`});
   
-  const data = JSON.stringify({content: `View Logged - IP: ${ip} - ${ua}`});
+  const webhook = 'https://discord.com/api/webhooks/1464006669796118610/NtLug816A6N5psWgUjMrUBFe-yRtvmPvmIC8hMrbORLuDrxP4QnErus1K5XH2pKlqXi9';
   
   try {
     const url = new URL(webhook);
@@ -14,18 +14,11 @@ module.exports = function handler(req, res) {
       hostname: url.hostname,
       path: url.pathname + url.search,
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': Buffer.byteLength(data)
-      }
+      headers: {'Content-Type': 'application/json', 'Content-Length': Buffer.byteLength(data)}
     };
-    
-    const req2 = https.request(options, (res2) => {});
-    req2.write(data);
-    req2.end();
+    https.request(options).end(data);
   } catch (e) {}
   
   res.setHeader('Content-Type', 'image/gif');
-  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-  res.status(200).send('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
+  res.send('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
 };
