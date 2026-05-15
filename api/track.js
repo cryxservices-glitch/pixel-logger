ab9347d74384e2f434c446bf2db95131f4ffb14c
cryxservices-glitch <cryxservices@gmail.com>
2026-05-15 01:55:19 -0400

    minimal working

--- api/track.js
+++ api/track.js
@@ -1,24 +1,4 @@
-const https = require('https');
-
-module.exports = function handler(req, res) {
-  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || 'unknown';
-  const ua = req.headers['user-agent'] || 'unknown';
-  
-  const data = JSON.stringify({content: `View ${ip}`});
-  
-  const webhook = 'https://discord.com/api/webhooks/1464006669796118610/NtLug816A6N5psWgUjMrUBFe-yRtvmPvmIC8hMrbORLuDrxP4QnErus1K5XH2pKlqXi9';
-  
-  try {
-    const url = new URL(webhook);
-    const options = {
-      hostname: url.hostname,
-      path: url.pathname + url.search,
-      method: 'POST',
-      headers: {'Content-Type': 'application/json', 'Content-Length': Buffer.byteLength(data)}
-    };
-    https.request(options).end(data);
-  } catch (e) {}
-  
+module.exports = async function(req, res) {
   res.setHeader('Content-Type', 'image/gif');
-  res.send('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
+  res.send(Buffer.from('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7', 'base64'));
 };
