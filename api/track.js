7919621d3a49e4d37061bd46c962188c108b2224
cryxservices-glitch <cryxservices@gmail.com>
2026-05-15 01:45:15 -0400

    trigger

--- api/track.js
+++ api/track.js
@@ -13,4 +13,4 @@ export default async function handler(req, res) {
   
   res.setHeader('Content-Type', 'image/gif');
   res.send(Buffer.from('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7', 'base64'));
-};
+};// redeploy trigger
